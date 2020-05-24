1721b322ca8b8f63963277b06169f3dbed94898f
ChiragSaini <chiragsaini7777@gmail.com>
2020-05-24 14:15:52 +0530

    Minor Change

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome TO React, MADAFAKA</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://chiragsaini.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Portfolio
         </a>
       </header>
     </div>
