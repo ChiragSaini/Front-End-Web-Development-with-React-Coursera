cced83e3b4cceab3a7004ba50bf38180ece2f373
Chirag Saini <chiragsaini7777@gmail.com>
2020-05-31 11:07:49 +0530

    Intro to Redux

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
 // * App turned into functional component form Class component.
 function App() {
+	const store = ConfigureStore();
 	return (
-		<BrowserRouter>
-			<div>
-				<Main />
-			</div >
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<div>
+					<Main />
+				</div >
+			</BrowserRouter>
+		</Provider>
 	);
 }
 
